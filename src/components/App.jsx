1f7711b2125368fbb43cfbcc62c95c8a9b4187f3
Vasylysaskyba <vasylysaskyba@gmail.com>
2023-07-16 23:18:35 +0300

    Container

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,14 +1,23 @@
+import { Container } from './Container/Container';
+
 import user from '../configs/user';
 import { Profile } from './Profile/Profile';
 
+// import data from '../configs/data';
+// import { Statistics } from './Statistics/Statistics';
+
 export const App = () => {
   return (
-    <Profile
-      username={user.username}
-      tag={user.tag}
-      location={user.location}
-      avatar={user.avatar}
-      stats={user.stats}
-    />
+    <Container>
+      <Profile
+        username={user.username}
+        tag={user.tag}
+        location={user.location}
+        avatar={user.avatar}
+        stats={user.stats}
+      />
+      {/* <Statistics title="Upload stats" stats={data} />
+      <Statistics stats={data} /> */}
+    </Container>
   );
 };
