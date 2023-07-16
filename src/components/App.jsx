e1b0a62d58d950e7669a89f5c1d91b33d8a53166
Vasylysaskyba <vasylysaskyba@gmail.com>
2023-07-16 23:19:34 +0300

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,8 +3,8 @@ import { Container } from './Container/Container';
 import user from '../configs/user';
 import { Profile } from './Profile/Profile';
 
-// import data from '../configs/data';
-// import { Statistics } from './Statistics/Statistics';
+import data from '../configs/data';
+import { Statistics } from './Statistics/Statistics';
 
 export const App = () => {
   return (
@@ -16,8 +16,8 @@ export const App = () => {
         avatar={user.avatar}
         stats={user.stats}
       />
-      {/* <Statistics title="Upload stats" stats={data} />
-      <Statistics stats={data} /> */}
+      <Statistics title="Upload stats" stats={data} />
+      <Statistics stats={data} />
     </Container>
   );
 };
